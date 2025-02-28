8178b5dc162096dd628c9619abef39775aa4862a
ashraful-fahim <ashraful30june@gmail.com>
2025-02-28 21:29:57 +0600

    made the transaction section functional for both cash out and add money

--- script/addMoney.js
+++ script/addMoney.js
@@ -5,27 +5,44 @@ document.getElementById('add-money-btn')
         const amount = getInputValueById('amount');
         const mainBalance = getInnerTextById('main-balance');
         const pin = getInputValueById('pin');
-        
-        if (account.length === 11){
-            if (pin === 1234){
+        const selectedBank = document.getElementById('all-bank').value;
+
+        if (amount < 0) {
+            alert('Please enter valid amount!');
+            return;
+        }
+
+        if (account.length === 11) {
+            if (pin === 1234) {
                 const sum = amount + mainBalance;
-                setElementText('main-balance',sum);
+                setElementText('main-balance', sum);
 
                 const transactionContainer = document.getElementById('transaction-container');
 
-                const p = document.createElement('p');
-                p.innerText = `
-                $${amount} is added from A/C No. ${account}.
-                -------------------------------------------
+                const div = document.createElement('div');
+                div.classList.add("bg-green-400");
+                div.innerHTML = `
+                <h1 class="text-yellow-300">Credited money from ${selectedBank}</h1>
+                <h3> Amount: ${amount}</h3>
+                <p> A/C Number: ${account}</p>
+                <p> New Balance: $${sum}</p>
                 `
+                transactionContainer.appendChild(div);
 
-                transactionContainer.appendChild(p);
+
+                // const p = document.createElement('p');
+                // p.innerText = `
+                // $${amount} is added from A/C No. ${account}.
+                // -------------------------------------------
+                // `
+
+                // transactionContainer.appendChild(p);
             }
-            else{
+            else {
                 alert('Wrong Password!!!')
             }
         }
-        else{
+        else {
             alert('Insert Valid Account Number!')
         }
     })
--- script/cashOut.js
+++ script/cashOut.js
@@ -3,32 +3,47 @@ document.getElementById('cash-out-btn')
         event.preventDefault();
 
         const account = document.getElementById('cashout-account-number').value;
-
         const amount = getInputValueById('cashout-amount');
-
         const pin = getInputValueById('cash-out-pin');
-
         const mainBalance = getInnerTextById('main-balance');
+        const selectedBank = document.getElementById('cashout-all-bank').value;
 
-        if (account.length === 11){
-            if(pin === 1234){
+        if (amount > mainBalance) {
+            alert('Insufficient funds!');
+            return;
+        }
+
+        if (account.length === 11) {
+            if (pin === 1234) {
                 sum = mainBalance - amount;
-                setElementText('main-balance',sum);   
+                setElementText('main-balance', sum);
 
                 const transactionContainer = document.getElementById('transaction-container');
-                
-                const p = document.createElement('p');
-                p.innerText = `
-                $${amount} is cashed out from A/C No. ${account}.
-                -------------------------------------------------
+
+                const div = document.createElement('div');
+                div.classList.add('bg-red-400');
+                div.innerHTML = `
+                <h1 class="text-yellow-300">Debited money from ${selectedBank}</h1>
+                <h3> Amount: ${amount}</h3>
+                <p> A/C Number: ${account}</p>
+                <p> Remaining Balance: $${sum}</p>
                 `
-                transactionContainer.appendChild(p);
+
+                transactionContainer.appendChild(div);
+
+
+                // const p = document.createElement('p');
+                // p.innerText = `
+                // $${amount} is cashed out from A/C No. ${account}.
+                // -------------------------------------------------
+                // `
+                // transactionContainer.appendChild(p);
             }
-            else{
+            else {
                 alert('Wrong PIN!!!')
             }
         }
-        else{
+        else {
             alert('Please enter a valid account number!')
         }
     })
